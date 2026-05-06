61ffae7b2a11e1f2b15996e486b649978c3fe28e
Draconic-Hacker <pythontesteemail55@gmail.com>
2026-05-06 16:42:52 -0300

    começando a corrigir os erros de importação (typescript)

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,12 +1,13 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import Initialization from '../components/HelloWorld.vue'
+// import Initialization from '../components/HelloWorld.vue'
 import Landing from '../views/landing.vue'
 import Test from '../views/test.vue'
 
 
 const routes = [
-    { path: '/', component: Initialization },
-    { path: '/landing', component: Landing },
+    { path: '/', component: Landing },
+    // { path: '/', component: Initialization },
+    // { path: '/landing', component: Landing },
     { path: '/test', component: Test },
     { path: '/:pathMatch(.*)*', redirect: '/' }
 ]
